98c64fcfb296ce30f22dfd0e1c6c0134c669b299
Lennart Duschek <lennart.duschek@css.de>
2021-12-03 08:22:17 +0100

    Added frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+
+    <h3>{{testText}}</h3>
+
+    <v-btn
+        color="deep-purple lighten-2"
+        text
+        @click="testFunction"
+      >
+        Lade Text
+  </v-btn>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+
+export default {
+  name: 'HelloWorld',
+  data: () => ({
+        testText: 'Das ist ein Test',
+  }), 
+  props: {
+    msg: String
+  },
+    methods: {
+      testFunction: function(){
+        //let urlParams = new URLSearchParams(window.location.search);
+            // console.log(urlParams.has('instance')); // true
+            // console.log(urlParams.get('instance')); // "MyParam" 
+        console.log("Hello")
+          axios
+            .get(process.env.VUE_APP_ROOT_API + 'helloWorld')
+            .then(res => {
+              this.testText = res.data.text
+              console.log(res)
+            })
+            .catch(error => {
+              // handle error
+              console.error(error);
+            })
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+module.exports = {
+  outputDir: path.resolve(__dirname, "../backend/dist"),
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
